0ec1f7d490c0c381f04588f51f7d6466c7882286
simonepietronostro <simonepietronostro@gmail.com>
2026-06-30 17:00:33 +0200

    Add initial app entrypoint

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const App = {
+  version: "0.1.0",
+  status: "initialized",
+  nextSteps: [
+    "Importare il codice operativo separato dalla V1",
+    "Verificare comportamento identico alla versione in uso",
+    "Avviare refactoring modulare",
+    "Aggiungere log modifiche e import Excel"
+  ]
+};
+
+console.info("Hotel Fletcher V2", App);
